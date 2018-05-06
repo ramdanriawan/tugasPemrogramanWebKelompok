df4886f157cf0175303c7499c532266003777bd0
ramdan riawan <ramdanriawan3@gmail.com>
2018-05-07 06:57:22 +0700

    pertama

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<?php $baseClientLibrary = $this->config->item("baseClientLibrary"); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>testing</title>
+        <link rel="stylesheet" href="<?php echo $baseClientLibrary . "/bootstrap/dist/css/bootstrap.min.css"; ?>">
+
+        <script src="<?php echo $baseClientLibrary . "/jquery/dist/jquery.min.js"; ?>" charset="utf-8"></script>
+        <script src="<?php echo $baseClientLibrary . "/bootstrap/dist/js/bootstrap.min.js"; ?>" charset="utf-8"></script>
+    </head>
